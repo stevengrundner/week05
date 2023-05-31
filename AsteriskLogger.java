4fa607be8aa66a0e5250465995d4e67a21471a37
xxxx <sginc982@gmail.com>
2023-05-31 14:54:19 -0600

    Questions 1-3 done

---
+++ AsteriskLogger.java
@@ -0,0 +1,19 @@
+package week05;
+
+public class AsteriskLogger implements Logger {
+
+	@Override
+	public void log(String log) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void error(String error) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
---
+++ Logger.java
@@ -0,0 +1,9 @@
+package week05;
+
+public interface Logger {
+
+	
+	public void log (String log);
+	public void error (String error);
+	
+}
